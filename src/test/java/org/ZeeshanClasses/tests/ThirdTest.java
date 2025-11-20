12358662cf65644a6d1e650d05d6c677bab2cd54
Zeeshan <zeeshanisme11@gmail.com>
2025-11-21 00:06:58 +0530

    another test case added for webhook check

---
+++ src/test/java/org/ZeeshanClasses/tests/ThirdTest.java
@@ -0,0 +1,17 @@
+package org.ZeeshanClasses.tests;
+
+import org.ZeeshanClasses.base.PrepBases;
+import org.ZeeshanClasses.customs.PrepProj;
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+public class ThirdTest extends PrepBases {
+    @Test
+    @PrepProj(id="TC004", description = "Another test to test webhooks", createdBy = "Zeeshan")
+    public void letsCallThisMethod() throws InterruptedException{
+        driver.navigate().to("https://www.facebook.com");
+        Thread.sleep(3000);
+        driver.findElement(By.linkText("meta")).click();
+        Thread.sleep(10000);
+    }
+}
